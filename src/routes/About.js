71f7e8cb5433c025e799cd9849321b8ebd9809b0
Donghyun Lee <ldhhello@naver.com>
2024-03-16 16:50:21 +0900

    소개 페이지 반응형

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo2 from '../images/logo2.png';
 import logo from '../images/logo.svg';
+import { useMediaQuery } from 'react-responsive';
 
 import "./About.css";
 
 export default function About() {
+    const isMobile = useMediaQuery({query: "(max-width:500px)"})
+
     return (
         <div className='about-div'>
             <img className='logo2' src={logo2}/>
             <img className='logo' src={logo}/>
-            <div>한국디지털미디어고등학교 익명 사진공유 서비스, 디미몬스터</div>
+            {!isMobile && <div>한국디지털미디어고등학교 익명 사진공유 서비스, 디미몬스터</div>}
+            {isMobile && <div>한국디지털미디어고등학교<br/>익명 사진공유 서비스</div>}
         </div>
     )
 }
