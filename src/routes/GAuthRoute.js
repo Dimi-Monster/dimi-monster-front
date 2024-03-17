b32cf6f4f393bb422c3b1463d2c1eda922335c9e
budung <gold@ksu.com>
2024-03-17 21:10:04 +0900

    vw또바꿨음

--- src/routes/GAuthRoute.js
+++ src/routes/GAuthRoute.js
@@ -38,7 +38,7 @@ export default function GAuthRoute() {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <img src={waitlogo} style={
-                { width: '38vw', height: 'auto' }
+                { width: '43vw', height: 'auto' }
             } alt="Wait Logo" />
         </div>
     )
