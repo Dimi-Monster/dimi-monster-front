bdb3e9188bb8a2035a40f7f770e0cfd8a2615a41
budung <gold@ksu.com>
2024-03-17 21:07:06 +0900

    Wait logo 추가했어여

--- src/routes/GAuthRoute.js
+++ src/routes/GAuthRoute.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react"
 import { useSearchParams, useNavigate } from "react-router-dom";
 import api from '../utils/API';
+import waitlogo from '../images/login_wait_logo.svg';
 
 export default function GAuthRoute() {
     const [searchParams, /*setSearchParams*/] = useSearchParams();
@@ -20,10 +21,13 @@ export default function GAuthRoute() {
             else {
                 let error = api.getLastError();
 
-                if(error == 'Invalid Email Address')
+                if(error == 'Invalid Email Address'){
                     alert('디미고 구글 계정으로만 로그인할 수 있습니다.');
-                else
+                } else if(error == 'Internal Server Error'){
+                    alert('로그인 정보를 불러오는 데 실패했습니다.');
+                } else {
                     alert('오류가 발생했습니다. 관리자에게 문의해주세요.\n\n에러 타입: '+error);
+                }
                 navigate('/login');
             }
         }
@@ -32,8 +36,10 @@ export default function GAuthRoute() {
     }, []);
 
     return (
-        <div>
-            로그인 중입니다...
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <img src={waitlogo} style={
+                { width: '10vw', height: 'auto' }
+            } alt="Wait Logo" />
         </div>
     )
 }
