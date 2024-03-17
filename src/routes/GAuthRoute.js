b67a3eb8c422e6c3716e878c55a2806362b58866
budung <gold@ksu.com>
2024-03-17 21:23:24 +0900

    코드 조금 더 보기 좋게 다듬었송

--- src/routes/GAuthRoute.js
+++ src/routes/GAuthRoute.js
@@ -38,7 +38,10 @@ export default function GAuthRoute() {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <img src={waitlogo} style={
-                { width: '50vw', height: 'auto' }
+                { 
+                    width: '50vw', 
+                    height: 'auto' 
+                }
             } alt="Wait Logo" />
         </div>
     )
