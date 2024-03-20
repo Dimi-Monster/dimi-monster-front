3ef4b48184de5396513640e38847edb872e4d7d2
Donghyun Lee <ldhhello@naver.com>
2024-03-20 16:18:21 +0900

    TitleBox 제목에 클래스 적용할 수 있게 수정

--- src/components/TitleBox.js
+++ src/components/TitleBox.js
@@ -4,7 +4,7 @@ import './TitleBox.css';
 export default function TitleBox(props) {
     return (
         <div className={`titlebox ${props.className}`}>
-            <div className="title">{props.title}</div>
+            <div className={`title ${props.titleClassName}`}>{props.title}</div>
 
             <div className={props.innerClassName} style={props.innerStyle}>
                 {props.children}
