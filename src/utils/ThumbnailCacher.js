4093994dc9c83e3dbdc7948c3897bd743c195f4a
Donghyun Lee <ldhhello@naver.com>
2024-03-25 11:47:19 +0900

    ⚡️ Cache image asynchronously

--- src/utils/ThumbnailCacher.js
+++ src/utils/ThumbnailCacher.js
@@ -25,41 +25,46 @@ class ThumbnailCacher {
 
         if(!res)
             return false;
-        
-        // 썸네일을 서버에서 불러오지 않은 경우, 썸네일이 전부 다 있는지 검증할 필요가 있다
-        if(!loadThumbnail) {
-            let isValid = true;
-
-            for(const k of res) {
-                if(!k.src && !(k.id in this.thumbnails)) {
-                    isValid = false;
-                    break;
-                }
-            }
-
-            // 클라이언트가 가진 데이터와 실제 데이터가 한 페이지 이상 차이 날 경우 드물게 실패할 수 있다
-            if(!isValid)
-                return this.getImage(pageIdx, true);
-        }
-
-        // 썸네일을 불러왔다면 캐싱하고, 불러오지 않았다면 캐시 데이터에서 썸네일을 가져온다
-        for(const k of res) {
-            if(k.src) {
-                if(!(k.id in this.thumbnails))
-                    this.thumbnailsCount++;
-
-                this.thumbnails[k.id] = k.src;
-            }
-            else
-                k.src = this.thumbnails[k.id];
-        }
 
         if(res.length < 21)
             this.lastPage = pageIdx;
         else if(this.lastPage == pageIdx)
             this.lastPage = -1;
 
+        if(loadThumbnail) {
+            this.cacheThumbnails(res).then(() => console.log('Cache finished'));
+            return res;
+        }
+
+        // 썸네일을 서버에서 불러오지 않은 경우, 썸네일이 전부 다 있는지 검증할 필요가 있다
+        let isValid = true;
+
+        for(const k of res) {
+            if(!k.src && !(k.id in this.thumbnails)) {
+                isValid = false;
+                break;
+            }
+        }
+
+        // 클라이언트가 가진 데이터와 실제 데이터가 한 페이지 이상 차이 날 경우 드물게 실패할 수 있다
+        if(!isValid)
+            return this.getImage(pageIdx, true);
+
+        // 캐시 데이터에서 썸네일을 가져온다
+        for(const k of res) {
+            k.src = this.thumbnails[k.id];
+        }
+
         return res;
+    }
+
+    async cacheThumbnails(data) {
+        for(const k of data) {
+            if(!(k.id in this.thumbnails))
+                this.thumbnailsCount++;
+
+            this.thumbnails[k.id] = k.src;
+        }
     }
 }
 
