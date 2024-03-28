064724cd3cfb247180842c04b2edd065909cd308
Donghyun Lee <ldhhello@naver.com>
2024-03-29 08:20:51 +0900

    ✨ Change Google logo to svg

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './GoogleLoginButton.css';
 import Button from './Button';
-import googlelogo from '../images/googlelogo.png';
+import googlelogo from '../images/googlelogo.svg';
 
 export default function GoogleLoginButton() {
 
     return (
-        <Button onClick={click} title="디미고 계정으로 로그인" imgSrc={googlelogo} height='1.4rem'/>
+        <Button onClick={click} title="디미고 계정으로 로그인" imgSrc={googlelogo} height='1.3rem'/>
     );
 
     function click() {
